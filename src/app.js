5009f607659b22ae39f57ff3bfcd18bcf7a0e802
xHenrySx <enrisal2000@gmail.com>
2023-01-15 03:48:52 -0300

    Fix some issues and add * route

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import bodyParser from "body-parser";
-import pkg from "../package.json" assert {type: "json"};
 import booksRouter from "./Routes/books.routes.js";
 import authRouter from "./Routes/auth.routes.js";
 import userRouter from "./Routes/user.routes.js";
@@ -12,21 +11,17 @@ createRoles();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.set("pkg", pkg);
 
-app.get("/", (req, res) => {
-    res.json({
-        author: app.get("pkg").author,
-        version: app.get("pkg").version,
-        name: app.get("pkg").name
-    });
-});
 
 app.use("/api/books", booksRouter);
 
 app.use("/api/auth", authRouter);
 
 app.use("/api/users", userRouter);
+
+app.get("*", (req, res) => {
+    res.status(404).json({ message: "Not found" });
+});
 
 
 export default app;
